1a96dc4fd3bf4bc2ce682d07d17333ec4483cec1
SamC95 <132593571+sJagstang@users.noreply.github.com>
2024-11-30 19:03:55 +0000

    feat: Implemented War Thunder news scraper
    Gets most recent post from war thunder news page
    Added thumbnail logos for Final Fantasy XI / War Thunder

---
+++ src/main/java/com/example/scraper/games/WarThunder.java
@@ -0,0 +1,88 @@
+package com.example.scraper.games;
+
+import com.example.scraper.data.DescriptionBuilder;
+import com.example.scraper.data.Patch;
+import com.example.scraper.data.PostBuilder;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+
+public class WarThunder {
+  private final Patch newsFeed;
+
+  public WarThunder() {
+    this.newsFeed = new Patch();
+  }
+
+  public static void main(String[] args) {
+    WarThunder warThunder = new WarThunder();
+
+    try {
+      warThunder.getNewsFeed();
+
+      PostBuilder.createNewsPost(warThunder.newsFeed);
+    }
+    catch (IOException error) {
+      System.err.println("Error: " + error.getMessage());
+    }
+  }
+
+  public void getNewsFeed() throws IOException {
+    String url = "https://warthunder.com/en/news";
+
+    var doc = connect(url);
+
+    Element entry = doc.selectFirst("div.showcase__item.widget");
+
+    if (entry != null) {
+      var title = entry.select("div.widget__title").text();
+      this.newsFeed.setTitle(title.isEmpty() ? "No title available" : title);
+
+      var postLink = entry.select("a.widget__link").attr("href");
+      this.newsFeed.setUrl(postLink.isEmpty() ? "No url found" : "https://warthunder.com" + postLink);
+
+      var widgetDiv = entry.selectFirst("div.widget__poster");
+      if (widgetDiv == null) {
+        this.newsFeed.setImage("No image found");
+        return;
+      }
+
+      var imgPath = widgetDiv.selectFirst("img.widget__poster-media");
+      if (imgPath == null) {
+        this.newsFeed.setImage("No image found");
+        return;
+      }
+
+      var imgUrl = imgPath.attr("data-src");
+      if (imgUrl.isEmpty()) {
+        imgUrl = imgPath.attr("src"); // If data-src is empty, try to fall back on src
+      }
+
+      if (imgUrl.startsWith("//")) {
+        imgUrl = "https:" + imgUrl;
+      }
+
+      this.newsFeed.setImage(imgUrl.isEmpty() ? "No image found" : imgUrl);
+
+      var description = entry.select("div.widget__comment").text();
+      description = Jsoup.parse(description).text();
+      description = DescriptionBuilder.truncateDescription(description, 300);
+      this.newsFeed.setDescription(
+          description.isEmpty() ? "No description available" : description);
+    }
+    else {
+      System.out.println("No entries found on warthunder.com");
+    }
+  }
+
+  private Document connect(String url) throws IOException {
+    try {
+      return Jsoup.connect(url).userAgent("Mozilla/5.0").get();
+    }
+    catch (IOException error) {
+      throw new IOException("Couldn't connect to warthunder.com: " + error.getMessage());
+    }
+  }
+}
